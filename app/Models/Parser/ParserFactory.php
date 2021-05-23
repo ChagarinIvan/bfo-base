5b9bfb245e3da0f8cd5224c54508a090322cd4b1
IVAN CHAHARYN <ivan.c@seranking.com>
2021-05-23 23:38:23 +0300

    Add Parser

--- app/Models/Parser/ParserFactory.php
+++ app/Models/Parser/ParserFactory.php
@@ -14,6 +14,7 @@ class ParserFactory
         WinOrientHtmlParser::class,
         OBelarusNetRelayWithHeadersParser::class,
         SimplyParser::class,
+//        OParser::class,
         OBelarusNetParser::class,
     ];
 
---
+++ tests/Integration/OParserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Integration;
+
+use App\Models\Parser\OParser;
+
+class OParserTest extends AbstractParserTest
+{
+    protected function getParser(): string
+    {
+        return OParser::class;
+    }
+
+    protected function getFilePath(): string
+    {
+        return '2021/20210402_mid_brest.html';
+    }
+
+    protected function getResults(): array
+    {
+        return [];
+    }
+
+    protected function geLinesCount(): int
+    {
+        return 207;
+    }
+}
