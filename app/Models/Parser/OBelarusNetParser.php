a6a4f475d91066dd8e50d7df43b146696bca4bed
IVAN CHAHARYN <ivan.c@seranking.com>
2021-08-30 20:58:01 +0300

    Update ObelarusNet Parser

--- app/Models/Parser/OBelarusNetParser.php
+++ app/Models/Parser/OBelarusNetParser.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Carbon;
 use RuntimeException;
+use function PHPUnit\Framework\isInstanceOf;
 
 class OBelarusNetParser implements ParserInterface
 {
@@ -19,19 +20,24 @@ class OBelarusNetParser implements ParserInterface
     {
         try {
             $doc = new DOMDocument();
-            @$doc->loadHTML($file->get());
+            $content = mb_convert_encoding($file->get(), 'utf-8', 'windows-1251');
+            @$doc->loadHTML($content);
             $xpath = new DOMXpath($doc);
             $preNodes = $xpath->query('//pre');
             $linesList = new Collection();
             foreach ($preNodes as $node) {
                 $text = trim($node->nodeValue);
                 $text = trim($text,'-');
+                $text = mb_convert_encoding($text, 'iso-8859-1', 'utf-8');
                 $text = trim($text);
                 if (!str_contains($text, 'амилия')) {
                     continue;
                 }
+                $distanceLength = 0;
+                $distancePoints = 0;
                 $groupNode = $xpath->query('preceding::h2[1]', $node);
                 $groupName = $groupNode[0]->nodeValue;
+                $groupName = mb_convert_encoding($groupName, 'iso-8859-1', 'utf-8');
                 if (str_contains($groupName, ',')) {
                     $groupName = substr($groupName, 0, strpos($groupName, ','));
                 }
@@ -42,10 +48,25 @@ class OBelarusNetParser implements ParserInterface
                 if ($linesCount < 3) {
                     continue;
                 }
-                $groupHeader = $lines[0];
-                $withPoints = str_contains($groupHeader, 'Oчки');
-                for ($index = 2; $index < $linesCount; $index++) {
+                $groupHeader = $lines[2];
+                $groupHeader = preg_replace('#\s+#', ' ', $groupHeader);
+                $groupHeaderData = explode(' ', $groupHeader);
+                $groupHeaderIndex = count($groupHeaderData) - 1;
+
+                if (preg_match('#(\d+)\s+[^\d]+,\s+((\d+,\d+)\s+[^\d]+|(\d+)\s+[^\d])#s', $lines[0], $match)) {
+                    $distancePoints = (int)$match[1];
+                    if (str_contains($match[2], ',')) {
+                        $distanceLength = floatval(str_replace(',', '.', $match[3])) * 1000;
+                    } else {
+                        $distanceLength = floatval($match[4]);
+                    }
+                }
+                for ($index = 4; $index < $linesCount; $index++) {
                     $line = trim($lines[$index]);
+
+//                    if (str_contains($line, 'Буковец')) {
+//                        sleep(1);
+//                    }
                     if (empty(trim($line, '-'))) {
                         break;
                     }
@@ -53,50 +74,20 @@ class OBelarusNetParser implements ParserInterface
                     $preparedLine = preg_replace('#\s+#', ' ', $preparedLine);
                     $lineData = explode(' ', $preparedLine);
                     $fieldsCount = count($lineData);
-                    $protocolLine = ['group' => $groupName];
+                    $protocolLine = [
+                        'group' => $groupName,
+                        'distance' => [
+                            'length' => $distanceLength,
+                            'points' => $distancePoints,
+                        ],
+                    ];
                     $indent = 1;
-                    if ($withPoints) {
-                        $points = $lineData[$fieldsCount - $indent++];
-                        if (is_numeric($points)) {
-                            $protocolLine['points'] = (int)$points;
-                        } elseif ($points === '-') {
-                            $protocolLine['points'] = null;
-                        } else {
-                            $points = $lineData[$fieldsCount - $indent++];
-                            $protocolLine['points'] = is_numeric($points) ? (int)$points : null;
+                    for ($i = $groupHeaderIndex; $i > 2; $i--) {
+                        $columnName = $this->getColumn($groupHeaderData[$i]);
+                        if ($columnName === '') {
+                            continue;
                         }
-                    }
-                    $completeRank = $lineData[$fieldsCount - $indent++];
-                    $isPP = str_starts_with($lineData[$fieldsCount - $indent], 'пп');
-                    if (is_numeric($completeRank)) {
-                        $protocolLine['complete_rank'] = '-';
-                        $place = $completeRank;
-                    } else {
-                        $protocolLine['complete_rank'] = $isPP ? '-' : $completeRank;
-                        $place = $lineData[$fieldsCount - $indent++];
-                    }
-                    $protocolLine['place'] = is_numeric($place) ? (int)$place : null;
-                    if ($isPP) {
-                        $time = null;
-                    } else {
-                        $time = null;
-                        try {
-                            $time = Carbon::createFromTimeString($lineData[$fieldsCount - $indent++]);
-                        } catch (\Exception $e) {
-                            $time = null;
-                        }
-                    }
-                    $protocolLine['time'] = $time;
-                    $year = $lineData[$fieldsCount - $indent++];
-                    $protocolLine['year'] = is_numeric($year) ? (int)$year : null;
-                    $protocolLine['runner_number'] = (int)$lineData[$fieldsCount - $indent++];
-                    if (!is_numeric($protocolLine['runner_number'])) {
-                        throw new RuntimeException('Что то не так с номером участника '.$preparedLine);
-                    }
-                    $rank = $lineData[$fieldsCount - $indent];
-                    if (Rank::validateRank($rank)) {
-                        $protocolLine['rank'] = $rank;
-                        $indent++;
+                        $protocolLine[$columnName] = $this->getValue($columnName, $lineData, $fieldsCount, $indent);
                     }
                     $protocolLine['serial_number'] = (int)$lineData[0];
                     $protocolLine['lastname'] = $lineData[1];
@@ -115,8 +106,85 @@ class OBelarusNetParser implements ParserInterface
 
     public function check(UploadedFile $file): bool
     {
-        $content = $file->get();
-        $result = preg_match('#pre>\n-#', $content, $m);
         return true;
+    }
+
+    private function getColumn(string $field): string
+    {
+        $field = mb_strtolower($field);
+        if (str_contains($field, 'чки')) {
+            return 'points';
+        }
+        if (str_contains($field, 'ып')) {
+            return 'complete_rank';
+        }
+        if (str_contains($field, 'есто')) {
+            return 'place';
+        }
+        if (str_contains($field, 'зультат')) {
+            return 'time';
+        }
+        if ($field ==='гр' || $field === 'г.р.') {
+            return 'year';
+        }
+        if (str_contains($field, 'омер')) {
+            return 'runner_number';
+        }
+        if (str_contains($field, 'вал')) {
+            return 'rank';
+        }
+        return '';
+    }
+
+    private function getValue(string $column, array $lineData, int $fieldsCount, int &$indent): mixed
+    {
+        if ($column === 'points') {
+            $points = $lineData[$fieldsCount - $indent++];
+            return is_numeric($points) ? (int)$points : null;
+        }
+        if ($column === 'complete_rank') {
+            return $lineData[$fieldsCount - $indent++];
+        }
+        if ($column === 'place') {
+            $place = $lineData[$fieldsCount - $indent];
+            if (is_numeric($place) || $place === '-') {
+                $indent++;
+                return  (int)$place;
+            }
+            return null;
+        }
+        if ($column === 'time') {
+            $time = $lineData[$fieldsCount - $indent++];
+            if (preg_match('#\d\d\.\d\d#', $time)) {
+                $indent++;
+                return null;
+            }
+            try {
+                $time = Carbon::createFromTimeString($time);
+            } catch (Exception) {
+                $time = null;
+            }
+            return $time;
+        }
+        if ($column === 'runner_number') {
+            return (int)$lineData[$fieldsCount - $indent++];
+        }
+        if ($column === 'rank') {
+            $rank = $lineData[$fieldsCount - $indent];
+            if (Rank::validateRank($rank)) {
+                $indent++;
+                return $rank;
+            }
+        }
+        if ($column === 'year') {
+            $year = $lineData[$fieldsCount - $indent];
+            if (is_numeric($year)) {
+                $indent++;
+                return (int)$year;
+            } else {
+                return null;
+            }
+        }
+        return null;
     }
 }
