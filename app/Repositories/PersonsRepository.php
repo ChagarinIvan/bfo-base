40ab96169b83e7f71165be746988c9570f817cac
IVAN CHAHARYN <ivan.c@seranking.com>
2021-09-14 19:53:44 +0300

    Update parser

--- app/Repositories/PersonsRepository.php
+++ app/Repositories/PersonsRepository.php
@@ -9,6 +9,14 @@ use Illuminate\Support\Collection;
 
 class PersonsRepository
 {
+    /**
+     * @return Collection|Person[]
+     */
+    public function getAll(): Collection
+    {
+        return Person::all();
+    }
+
     public function getPersonsOrderedByProtocolLinesCountQuery(): Builder
     {
         return Person::join('protocol_lines', 'protocol_lines.person_id', '=', 'person.id')
