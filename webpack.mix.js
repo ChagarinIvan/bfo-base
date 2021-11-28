ed3ac9fecc00c3ac6146c1af601a31b3ed8f5004
IVAN CHAHARYN <ivan.c@seranking.com>
2021-11-28 14:42:50 +0200

    Start use laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.autoload({
+    '@popper.js/core/dist/umd/popper.js': ['Popper']
+});
+mix.js('resources/js/app.js', 'public/js');
+mix.sass('resources/css/app.scss', 'public/css');
