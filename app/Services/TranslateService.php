1141d747868aef85a519ef14e6fe9138b1b389b0
ChagarinIvan <Chagarin.Ivan@gmail.com>
2022-11-15 23:07:05 +0200

    Add google translator

--- app/Services/TranslateService.php
+++ app/Services/TranslateService.php
@@ -3,17 +3,22 @@
 namespace App\Services;
 
 use Illuminate\Cache\Repository as CacheManager;
+use Google\Cloud\Translate\V2\TranslateClient;
 
 class TranslateService
 {
+    private readonly TranslateClient $translator;
+
     public function __construct(private readonly CacheManager $cache)
-    {}
+    {
+        $this->translator = new TranslateClient(['key' => env('GOOGLE_AUTH_KEY')]);
+    }
 
     public function translate(string $value, string $locale = UserService::BY_LOCALE): string
     {
         return $this->cache->rememberForever(
             crc32($value),
-            fn () => $value
+            fn () => $this->translator->translate($value, ['target' => $locale])['text'] ?? $value
         );
     }
 }
