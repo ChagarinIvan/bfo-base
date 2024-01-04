a81b6451e27491c92fb5386dcba88770966afa9a
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-01-04 20:31:44 +0200

    test

--- resources/views/ranks/show-person-ranks.blade.php
+++ resources/views/ranks/show-person-ranks.blade.php
@@ -93,9 +93,10 @@
                                         <label for="start_date">{{ __('app.common.date') }}</label>
                                     </div>
                                 </div>
-                                <div class="modal-footer">
-                                    <input type="submit" class="btn btn-outline-primary btn-sm" value="{{ __('app.rank.submit') }}">
-                                </div>
+                                <input type="submit" class="btn btn-outline-primary btn-sm" value="{{ __('app.rank.submit') }}">
+
+{{--                                <div class="modal-footer">--}}
+{{--                                </div>--}}
                             </form>
                         </div>
                     </div>
