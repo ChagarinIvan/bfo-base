bc31676eb7b0c09fe83490bd4933a80a74b15689
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-02-04 00:32:05 +0200

    fix cup export

--- app/Http/Controllers/Cups/ExportCupTableAction.php
+++ app/Http/Controllers/Cups/ExportCupTableAction.php
@@ -16,21 +16,24 @@ class ExportCupTableAction extends AbstractCupAction
 {
     public function __invoke(Cup $cup): BinaryFileResponse
     {
-        $tempFilePath = tempnam(sys_get_temp_dir(), 'tempfile');
-
+        $content = '';
         foreach ($cup->getCupType()->getGroups() as $group) {
             $cupEvents = $this->cupEventsService->getCupEvents($cup)->sortBy('event.date');
             $cupPoints = $this->cupEventsService->calculateCup($cup, $cupEvents, $group);
 
-            $view = $this->view('cup.export.table', [
-                'cup' => $cup,
-                'cupEvents' => $cupEvents,
-                'cupPoints' => $cupPoints,
-                'persons' => Person::whereIn('id', array_keys($cupPoints))->get()->keyBy('id'),
-            ]);
+            $content .= $this
+                ->view('cup.export.table', [
+                    'cup' => $cup,
+                    'cupEvents' => $cupEvents,
+                    'cupPoints' => $cupPoints,
+                    'persons' => Person::whereIn('id', array_keys($cupPoints))->get()->keyBy('id'),
+                ])
+                ->render()
+            ;
 
-            File::put($tempFilePath, $view->render());
         }
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'tempfile');
+        File::put($tempFilePath, $content);
 
         return response()
             ->download(file: $tempFilePath, name: "$cup->name.html")
