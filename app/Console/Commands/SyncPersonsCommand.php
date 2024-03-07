2b209aebd32e8b64d76b60eb78c14b117555d14b
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-07 16:56:42 +0100

    fix

--- app/Console/Commands/SyncPersonsCommand.php
+++ app/Console/Commands/SyncPersonsCommand.php
@@ -31,6 +31,9 @@ class SyncPersonsCommand extends Command
 
         $persons = $apiClient->getPersons();
         foreach ($persons as $index => $person) {
+            if (!str_contains($person->name, 'Буковец')) {
+                continue;
+            }
             $this->storage->put('/sync/' . $index, serialize($person));
         }
     }
