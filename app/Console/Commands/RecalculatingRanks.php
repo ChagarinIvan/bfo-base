4536ce9bc4a8fc5f526aede57527717375418e8b
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-10 23:28:27 +0100

    add logger

--- app/Console/Commands/RecalculatingRanks.php
+++ app/Console/Commands/RecalculatingRanks.php
@@ -4,24 +4,37 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Integration\OrientBy\OrientBySyncService;
 use App\Models\ProtocolLine;
 use App\Models\Rank;
 use App\Services\RankService;
 use Illuminate\Console\Command;
+use Illuminate\Log\LogManager;
+use Psr\Log\LoggerInterface;
 
 /**
  * Пересоздаем разряды.
  */
-class RecalculatingRanks extends Command
+final class RecalculatingRanks extends Command
 {
     protected $signature = 'protocol-lines:rank-recalculating';
+    private readonly LoggerInterface $logger;
 
+    public function __construct(
+        LogManager $loggerManager,
+    ) {
+        parent::__construct();
+        $this->logger = $loggerManager->channel('ranks');
+    }
     public function handle(RankService $service): void
     {
+        $this->logger->info('Start.');
+
         Rank::truncate();
-        foreach (ProtocolLine::all() as $protocolLine) {
+        foreach (ProtocolLine::all() as $index => $protocolLine) {
+            $this->logger->info("Process $index.");
             $service->fillRank($protocolLine);
         }
-        $this->info("Finish");
+        $this->logger->info('Finish.');
     }
 }
