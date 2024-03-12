6fc4380f9b1d06b981988d18c36be7d7314eeacd
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-12 23:25:20 +0100

    fix impression date

--- resources/views/components/impression.blade.php
+++ resources/views/components/impression.blade.php
@@ -4,12 +4,10 @@
      * @var string $email
      */
 @endphp
-<a href="">
-    <span style="margin-right: 5px;">{{ $date }}</span>
-    <i class="bi bi-info-circle-fill text-info"
-       data-bs-toggle="tooltip"
-       data-bs-placement="top"
-       title="{{ $email }}"
-    ></i>
-</a>
+<span style="margin-right: 5px;">{{ $date }}</span>
+<i class="bi bi-info-circle-fill text-info"
+   data-bs-toggle="tooltip"
+   data-bs-placement="top"
+   title="{{ $email }}"
+></i>
 
