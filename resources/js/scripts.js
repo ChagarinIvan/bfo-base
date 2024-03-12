9cd33ec46f3c12d46d0405be6cd58560f243bad0
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-13 00:12:44 +0100

    fix

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,13 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var table = document.querySelector('#table');
-    $(table).on('post-body.bs.table', function (e, data) {
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        });
-    });
-    // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    //     return new bootstrap.Tooltip(tooltipTriggerEl)
-    // });
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 });
--- resources/views/components/impression.blade.php
+++ resources/views/components/impression.blade.php
@@ -4,9 +4,11 @@
      * @var string $email
      */
 @endphp
-<span style="margin-right: 5px;">{{ $date }}</span>
-<i class="bi bi-info-circle-fill text-info"
-   data-bs-toggle="tooltip"
-   data-bs-placement="top"
-   title="{{ $email }}"
-></i>
+<td data-toggle="tooltip" title="{{ $email }}">
+    <span style="margin-right: 5px;">{{ $date }}</span>
+    <i class="bi bi-info-circle-fill text-info"
+       data-bs-toggle="tooltip"
+       data-bs-placement="top"
+       title="{{ $email }}"
+    ></i>
+</td>
