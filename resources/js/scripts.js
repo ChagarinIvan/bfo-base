ea2fc860efd9f62e044596139bf2c5f73026525c
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-13 00:14:58 +0100

    fix

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,3 +2,12 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var table = document.querySelector('#table');
+    $(table).bootstrapTable({
+        onPostBody: function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        }
+    });
+});
