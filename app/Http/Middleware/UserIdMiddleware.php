1ec1ded1810c44717c30203f9e4bf914bd97c80d
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-13 23:25:33 +0100

    fix middleware

--- app/Http/Middleware/UserIdMiddleware.php
+++ app/Http/Middleware/UserIdMiddleware.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Auth\SessionGuard;
+use Illuminate\Contracts\Auth\Factory as AuthService;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final readonly class UserIdMiddleware
 {
-    public function __construct(private SessionGuard $auth)
+    public function __construct(private AuthService $auth)
     {
     }
 
     public function handle(Request $request, Closure $next): Response
     {
-        $userId = $this->auth->id();
+        $userId = $this->auth->guard()->id();
         $request->merge(['userId' => $userId]);
 
         return $next($request);
