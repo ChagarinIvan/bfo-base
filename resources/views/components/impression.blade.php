b05f032ea70365377749434bd8202c8151893ed9
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-03-13 10:08:08 +0100

    fix

--- resources/views/components/impression.blade.php
+++ resources/views/components/impression.blade.php
@@ -4,8 +4,8 @@
      * @var string $email
      */
 @endphp
+{{ $date }}
 <div class="tooltip">
-    <span style="margin-right: 5px;">{{ $date }}</span>
     <i class="impression-icon bi bi-info-circle-fill"></i>
     <span class="tooltip-text">{{ $email }}</span>
 </div>
