3ad23d1c8cb96216a22398b2605cde57b0f1fa05
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-05-20 19:38:05 +0300

    fix cups

--- app/Domain/Cup/CupType/EliteCupType.php
+++ app/Domain/Cup/CupType/EliteCupType.php
@@ -19,8 +19,8 @@ use function in_array;
 
 class EliteCupType extends AbstractCupType
 {
-    public const ELITE_MEN_GROUPS = ['М21Е', 'М21E', 'МЕ', 'MЕ', 'Мужчины группа Е', 'М21', 'M21A', 'M21А', 'М21A', 'М21А', 'M21E', 'МE', 'М21 Фин Е', 'M21', 'МE(35)', 'М35', 'M35', 'М40', 'M40'];
-    public const ELITE_WOMEN_GROUPS = ['Ж21', 'Ж21Е', 'W21', 'ЖЕ', 'ЖE', 'Ж21E', 'W21E', 'W21A', 'W21А', 'Ж21A', 'Ж21А', 'Ж21 Фин Е', 'Женщины группа Е', 'ЖE(35)', 'Ж35', 'Ж40', 'W35', 'W40'];
+    public const ELITE_MEN_GROUPS = ['М21Е', 'М21E', 'МЕ', 'MЕ', 'Мужчины группа Е', 'М21', 'M21E', 'МE', 'М21 Фин Е', 'M21', 'МE(35)', 'М35', 'M35', 'М40', 'M40'];
+    public const ELITE_WOMEN_GROUPS = ['Ж21', 'Ж21Е', 'W21', 'ЖЕ', 'ЖE', 'Ж21E', 'W21E', 'Ж21 Фин Е', 'Женщины группа Е', 'ЖE(35)', 'Ж35', 'Ж40', 'W35', 'W40'];
 
     private static array $map = [];
 
@@ -86,12 +86,12 @@ class EliteCupType extends AbstractCupType
                 (new CupGroup(GroupMale::Man))->id() => array_merge(
                     self::ELITE_MEN_GROUPS,
                     JuniorCupType::MEN_MAIN_GROUPS_NAMES,
-                    ['M18', 'М18']
+                    ['M18', 'М18', 'M21A', 'M21А', 'М21A', 'М21А',]
                 ),
                 (new CupGroup(GroupMale::Woman))->id() => array_merge(
                     self::ELITE_WOMEN_GROUPS,
                     JuniorCupType::WOMEN_MAIN_GROUPS_NAMES,
-                    ['Ж18', 'W18']
+                    ['Ж18', 'W18', 'W21A', 'W21А', 'Ж21A', 'Ж21А',]
                 )
             ];
         }
