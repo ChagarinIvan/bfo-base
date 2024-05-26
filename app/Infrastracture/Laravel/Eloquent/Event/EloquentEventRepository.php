55a1b8103bdb5e7acf771bb1521b223fd709dbef
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-05-26 20:48:38 +0300

    dump sql

--- app/Infrastracture/Laravel/Eloquent/Event/EloquentEventRepository.php
+++ app/Infrastracture/Laravel/Eloquent/Event/EloquentEventRepository.php
@@ -83,6 +83,7 @@ final class EloquentEventRepository implements EventRepository
             $query->orderBy('date', 'asc');
         }
 
+        dump($query->sql());
         return $query;
     }
 }
