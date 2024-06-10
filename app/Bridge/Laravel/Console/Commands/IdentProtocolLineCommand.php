cf847da24a628fcfd2f889455858b65869abedb6
ChagarinIvan <Chagarin.Ivan@gmail.com>
2024-06-10 22:30:31 +0300

    Fix year command

--- app/Bridge/Laravel/Console/Commands/IdentProtocolLineCommand.php
+++ app/Bridge/Laravel/Console/Commands/IdentProtocolLineCommand.php
@@ -71,7 +71,7 @@ final class IdentProtocolLineCommand extends Command
             $personInfo = new PersonInfoDto;
             $personInfo->lastname = $protocolLine->lastname;
             $personInfo->firstname = $protocolLine->firstname;
-            $personInfo->birthday = $protocolLine->year ? (string) $protocolLine->year : null;
+            $personInfo->birthday = $protocolLine->year ? sprintf('%d-01-01', $protocolLine->year)  : null;
             $personInfo->clubId = $club ? (string) $club->id : null;
             $personInfo->citizenship = Citizenship::BELARUS->value;
             $personDto = new PersonDto;
