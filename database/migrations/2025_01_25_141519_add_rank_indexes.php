78803b4787c3a5b69d08c7c66d9ed260372e9f37
ChagarinIvan <Chagarin.Ivan@gmail.com>
2025-01-25 17:04:38 +0200

    Add indexes

---
+++ database/migrations/2025_01_25_141519_add_rank_indexes.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ranks', static function (Blueprint $table): void {
+            $table->index('person_id');
+            $table->index('finish_date');
+            $table->index('start_date');
+            $table->index('activated_date');
+        });
+
+        Schema::table('person', static function (Blueprint $table): void {
+            $table->index('active');
+        });
+
+        Schema::table('protocol_lines', static function (Blueprint $table): void {
+            $table->index('person_id');
+            $table->index('prepared_line');
+            $table->index('distance_id');
+            $table->index('activate_rank');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ranks', static function (Blueprint $table): void {
+            $table->dropIndex('person_id');
+            $table->dropIndex('finish_date');
+            $table->dropIndex('start_date');
+            $table->dropIndex('activated_date');
+        });
+
+        Schema::table('person', static function (Blueprint $table): void {
+            $table->dropIndex('active');
+        });
+
+        Schema::table('protocol_lines', static function (Blueprint $table): void {
+            $table->dropIndex('person_id');
+            $table->dropIndex('prepared_line');
+            $table->dropIndex('distance_id');
+            $table->dropIndex('activate_rank');
+        });
+    }
+};
