2f4e0fee43c062e4c2223e3e21e1cebf94c13eaa
ChagarinIvan <Chagarin.Ivan@gmail.com>
2025-01-25 15:24:30 +0200

    Add remove rank handler

---
+++ app/Application/Handler/Rank/RemoveRanksOnDisablePersonHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Handler\Rank;
+
+use App\Domain\Person\Event\PersonDisabled;
+use App\Domain\Rank\Rank;
+use App\Domain\Rank\RankRepository;
+use App\Domain\Shared\Criteria;
+
+final readonly class RemoveRanksOnDisablePersonHandler
+{
+    public function __construct(private RankRepository $ranks)
+    {
+    }
+
+    public function handle(PersonDisabled $event): void
+    {
+        /** @var Rank $rank */
+        foreach ($this->ranks->byCriteria(new Criteria(['person_id' => $event->person->id])) as $rank) {
+            $rank->delete();
+        }
+    }
+}
