6514df1925ce01bf36c6339dfa2d50276aa512b7
ChagarinIvan <Chagarin.Ivan@gmail.com>
2025-03-03 00:18:31 +0200

    add sorting

--- app/Application/Service/Rank/PersonRanks.php
+++ app/Application/Service/Rank/PersonRanks.php
@@ -14,6 +14,6 @@ final readonly class PersonRanks
 
     public function criteria(): Criteria
     {
-        return new Criteria(['person_id' => $this->personId], ['start_date' => 'asc']);
+        return new Criteria(['person_id' => $this->personId], ['start_date' => 'desc']);
     }
 }
