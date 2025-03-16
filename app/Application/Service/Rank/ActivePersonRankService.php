1fce7d516017916ceb3796250ebb362d4fb80784
ChagarinIvan <chagarin.ivan@gmail.com>
2025-03-16 13:45:41 +0200

    dump fill new rank

--- app/Application/Service/Rank/ActivePersonRankService.php
+++ app/Application/Service/Rank/ActivePersonRankService.php
@@ -39,6 +39,7 @@ final readonly class ActivePersonRankService
 
         if (!$lastRank) {
             $lastCompletedRank = $this->ranks->oneByCriteria($this->criteriaWithoutDate($command));
+            dump('$lastCompletedRank: ' . $lastCompletedRank->id);
             if ($lastCompletedRank) {
                 $lastRank = $this->previousCompletedRankFiller->fill($lastCompletedRank, $command->date());
             }
