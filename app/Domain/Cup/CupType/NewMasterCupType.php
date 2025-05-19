980960e0ff895fc3045c4f06b7ad585666130ff3
ChagarinIvan <chagarin.ivan@gmail.com>
2025-05-19 22:52:35 +0300

    New master cup

--- app/Domain/Cup/CupType/NewMasterCupType.php
+++ app/Domain/Cup/CupType/NewMasterCupType.php
@@ -31,7 +31,7 @@ use Illuminate\Support\Collection;
 class NewMasterCupType extends AbstractCupType
 {
     protected const GROUPS_MAP = [
-        'M_35_' => ['M35', 'М35', 'МE(35)'],
+        'M_35_' => ['M35', 'М35'],
         'M_40_' => ['M40', 'М40'],
         'M_45_' => ['M45', 'М45'],
         'M_50_' => ['M50', 'М50'],
@@ -41,7 +41,7 @@ class NewMasterCupType extends AbstractCupType
         'M_70_' => ['M70', 'М70'],
         'M_75_' => ['M75', 'М75'],
         'M_80_' => ['M80', 'М80'],
-        'W_35_' => ['Ж35', 'W35', 'ЖE(35)'],
+        'W_35_' => ['Ж35', 'W35'],
         'W_40_' => ['Ж40', 'W40'],
         'W_45_' => ['Ж45', 'W45'],
         'W_50_' => ['Ж50', 'W50'],
