a9d9a1a6fd59a984f3dfebdc3cf4fc5516ed821a
ChagarinIvan <chagarin.ivan@gmail.com>
2025-11-03 22:28:01 +0200

    Add dd

--- app/Bridge/Laravel/Http/Controllers/Cup/ShowCupsListAction.php
+++ app/Bridge/Laravel/Http/Controllers/Cup/ShowCupsListAction.php
@@ -20,7 +20,7 @@ class ShowCupsListAction extends BaseController
         ListCupService $service,
     ): View {
         $cups = $service->execute(new ListCup($search));
-dd($cups);
+
         /** @see /resources/views/cup/index.blade.php */
         return $this->view('cup.index', [
             'selectedYear' => (int) ($search->year ?? Year::actualYear()->value),
