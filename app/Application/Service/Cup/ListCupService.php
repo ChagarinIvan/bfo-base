ecc87e82eaaf6d4b1f00cb839f21f38cdc449708
ChagarinIvan <chagarin.ivan@gmail.com>
2025-11-03 22:42:07 +0200

    Add dd

--- app/Application/Service/Cup/ListCupService.php
+++ app/Application/Service/Cup/ListCupService.php
@@ -24,7 +24,7 @@ final readonly class ListCupService
         $all = $this->cups->byCriteria($command->criteria())->all();
         $views = [];
 
-        for ($i = 0; $i < 2; $i++) {
+        for ($i = 0; $i < 4; $i++) {
             $views[] = $this->assembler->toViewCupDto($all[$i]);
         }
 
